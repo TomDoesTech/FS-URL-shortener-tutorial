0e9753fcd04a22026d288059c25b7ccfa4e2d615
Pablo Emanuel <pablo.emanuel@cactussystem.com.br>
2022-02-18 10:48:26 -0300

    url-encurtador

--- server/build/src/schemas/createShortUrl.schema.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var yup_1 = require("yup");
-exports.default = yup_1.object({
-    destination: yup_1.string().required("Destination is required"),
-});
